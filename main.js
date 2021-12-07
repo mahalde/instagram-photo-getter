e152739e2c4cb5f490d95bab5963adbe46424c33
mahalde <github@malte-thienel.de>
2021-12-07 21:33:09 +0100

    Small changes

--- main.js
+++ main.js
@@ -1,21 +1,26 @@
 const Instagram = require("instagram-web-api");
 const Downloader = require("nodejs-file-downloader");
-
-const client = new Instagram({
-  username: "username",
-  password: "password",
-});
-const username = "username";
+const FileCookieStore = require('tough-cookie-filestore2');
+require('dotenv').config();
 
 async function main() {
-  await client.login();
-  const { user } = await client
-    .getPhotosByUsername({ username })
-    .catch(console.error);
+  const cookieStore = new FileCookieStore('./cookies.json');
+  const instaUser = {
+    username: process.env.USERNAME,
+    password: process.env.PASSWORD,
+  };
+
+  const client = new Instagram({
+    ...instaUser,
+    cookieStore
+  });
+  const username = process.env.PHOTO_USER;
+
+  await client.login(instaUser);
+  const { user } = await client.getPhotosByUsername({ username});
   let photoObj = user.edge_owner_to_timeline_media;
-  
+
   let photoUrls = await getPictures(photoObj);
-  console.log('worked');
 
   while (photoObj.page_info.has_next_page) {
     photoObj = (
@@ -26,17 +31,18 @@ async function main() {
     ).user.edge_owner_to_timeline_media;
 
     photoUrls = [...photoUrls, ...(await getPictures(photoObj))];
-    console.log('worked');
   }
 
-  console.log(photoUrls);
-  for (const url of photoUrls) {
+  const pictureAmount = photoUrls.length;
+    
+  for (let i = 0; i < pictureAmount; i++) {
     try {
       const downloader = new Downloader({
-        url,
-        directory: './instagram'
+        url: photoUrls[i],
+        directory: `./instagram-${username}`
       });
       await downloader.download();
+      console.log(`Downloaded media ${i + 1} of a total ${pictureAmount}`);
     } catch (err) {
       console.error(err);
       break;
@@ -47,25 +53,27 @@ async function main() {
 async function getPictures(photoObj) {
   const edges = photoObj.edges;
   const photoUrls = [];
-  console.log(edges.length);
+  console.log(`Getting media URLs of ${edges.length} posts`);
 
-  for (const {node} of edges) {
+  for (const { node } of edges) {
     const children = node.edge_sidecar_to_children;
 
     if (!children) {
       if (node.is_video) {
         photoUrls.push(node.video_url)
+      } else {
+        photoUrls.push(node.display_url)
       }
-      photoUrls.push(node.display_url)
       continue;
     }
     const childEdges = children.edges;
 
-    for (const {node: childNode} of childEdges) {
+    for (const { node: childNode } of childEdges) {
       if (childNode.is_video) {
         photoUrls.push(childNode.video_url);
+      } else {
+        photoUrls.push(childNode.display_url);
       }
-      photoUrls.push(childNode.display_url);
     }
   }
 
